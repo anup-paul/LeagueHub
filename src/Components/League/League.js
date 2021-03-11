5deb20edc8b93c17d23ee952670f4bea586e2c5b
anup-paul <anuppaul1845@gmail.com>
2021-03-11 16:08:30 +0600

    done

--- src/Components/League/League.js
+++ src/Components/League/League.js
@@ -1,4 +1,4 @@
-// import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useHistory } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
@@ -6,26 +6,26 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const League = (props) => {
     const { strLeagueAlternate, strLeague, idLeague } = props.league;
-    // const [details, setDetails] = useState([]);
-    // console.log(details);
+    const [details, setDetails] = useState([]);
+    console.log(details);
     const history = useHistory()
     const handleClick = (id) => {
         history.push(`/league/${id}`)
     }
-    // useEffect(()=>
-    // {
-    //     const url = (`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${idLeague}`)
-    //     fetch(url)
-    //     .then(res => res.json())
-    //     .then(data => setDetails(data.leagues))
-    // }, [idLeague])
+    useEffect(()=>
+    {
+        const url = (`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${idLeague}`)
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setDetails(data.leagues))
+    }, [idLeague])
     return (
         
             <div className="col-md-4 text-center" >
                 <div className="card cardDesign" style={{ width: "18rem", height: "300px", marginBottom: "30px",marginTop:"30px",backgroundColor:"steelblue" }}>
-                   {/* {
+                   {
                        details.map(details => <img src={details.strLogo}alt=""/>)
-                   } */}
+                   }
                     
                     <div className="card-body ">
                         <h5 className="card-title">{strLeagueAlternate}</h5>
