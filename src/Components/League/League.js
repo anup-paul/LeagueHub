e625a66117d86b40f0a8e4233db2d8425f324c20
anup-paul <anuppaul1845@gmail.com>
2021-03-11 13:37:49 +0600

    done

--- src/Components/League/League.js
+++ src/Components/League/League.js
@@ -6,7 +6,7 @@ import { useState } from 'react/cjs/react.development';
 
 const League = (props) => {
     const { strLeagueAlternate, strLeague, idLeague } = props.league;
-    const [details, setDetails] = useState({});
+    const [details, setDetails] = useState([]);
     // console.log(details);
     const history = useHistory()
     const handleClick = (id) => {
@@ -17,13 +17,16 @@ const League = (props) => {
         const url = (`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${idLeague}`)
         fetch(url)
         .then(res => res.json())
-        .then(data => setDetails(data.leagues[0]))
+        .then(data => setDetails(data))
     }, [idLeague])
     return (
         
             <div className="col-md-4 text-center" >
                 <div className="card cardDesign" style={{ width: "18rem", height: "300px", marginBottom: "30px",marginTop:"30px",backgroundColor:"steelblue" }}>
-                    <img src={details.strLogo}alt=""/>
+                   {
+                       details.map(details => <img src={details.strLogo}alt=""/>)
+                   }
+                    {/* <img src={details.strLogo}alt=""/> */}
                     <div className="card-body ">
                         <h5 className="card-title">{strLeagueAlternate}</h5>
                         <h6 className="card-subtitle mb-2 text-dark">{strLeague}</h6>
