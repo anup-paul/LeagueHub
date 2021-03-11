2ef41920edf4ac5d4ec928132f1f7418e11b81fb
anup-paul <anuppaul1845@gmail.com>
2021-03-11 13:54:47 +0600

    done

--- src/Components/League/League.js
+++ src/Components/League/League.js
@@ -6,27 +6,27 @@ import { useState } from 'react/cjs/react.development';
 
 const League = (props) => {
     const { strLeagueAlternate, strLeague, idLeague } = props.league;
-    const [details, setDetails] = useState([]);
+    // const [details, setDetails] = useState([]);
     // console.log(details);
     const history = useHistory()
     const handleClick = (id) => {
         history.push(`/league/${id}`)
     }
-    useEffect(()=>
-    {
-        const url = (`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${idLeague}`)
-        fetch(url)
-        .then(res => res.json())
-        .then(data => setDetails(data.leagues))
-    }, [idLeague])
+    // useEffect(()=>
+    // {
+    //     const url = (`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${idLeague}`)
+    //     fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => setDetails(data.leagues))
+    // }, [idLeague])
     return (
         
             <div className="col-md-4 text-center" >
                 <div className="card cardDesign" style={{ width: "18rem", height: "300px", marginBottom: "30px",marginTop:"30px",backgroundColor:"steelblue" }}>
-                   {
+                   {/* {
                        details.map(details => <img src={details.strLogo}alt=""/>)
-                   }
-                    {/* <img src={details.strLogo}alt=""/> */}
+                   } */}
+                    
                     <div className="card-body ">
                         <h5 className="card-title">{strLeagueAlternate}</h5>
                         <h6 className="card-subtitle mb-2 text-dark">{strLeague}</h6>
