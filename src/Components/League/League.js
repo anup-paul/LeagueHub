fdcacbb154a2d59384cb9baac9d0190912eff753
anup-paul <anuppaul1845@gmail.com>
2021-03-11 13:56:46 +0600

    done

--- src/Components/League/League.js
+++ src/Components/League/League.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react/cjs/react.development';
+// import { useState } from 'react/cjs/react.development';
 
 const League = (props) => {
     const { strLeagueAlternate, strLeague, idLeague } = props.league;
